5300b837d69dabb7b72cef622edaafbe4adff4df
Eyal Zekaria <eyal.z@doit-intl.com>
2023-02-24 11:00:31 +0100

    fixup! Add TF configuration

--- infrastructure/cloudbuild.tf
+++ infrastructure/cloudbuild.tf
@@ -3,6 +3,18 @@ locals {
   docker_image = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.telemetry_collector.name}/collector:$COMMIT_SHA"
 }
 
+resource "google_project_iam_member" "cloubuild_run_admin" {
+  project = var.project_id
+  role    = "roles/run.admin"
+  member  = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "cloubuild_sa_user" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
+}
+
 resource "google_cloudbuild_trigger" "telemetry_pipeline" {
   location = var.region
   name     = "telemetry-pipeline"
@@ -32,7 +44,7 @@ resource "google_cloudbuild_trigger" "telemetry_pipeline" {
       args = ["run", "deploy", local.service_name, "--image", local.docker_image,
         "--region", var.region, "--vpc-connector", google_vpc_access_connector.connector.name,
         "--set-env-vars=DATABASE_HOST=${google_sql_database_instance.telemetry.private_ip_address},DATABASE_USER=${google_sql_user.telemetry_user.name}",
-        "--set-secrets=DATABASE_PASSWORD=${google_secret_manager_secret_version.telemetry_db.id}:${google_secret_manager_secret_version.telemetry_db.version}"
+        "--set-secrets=DATABASE_PASSWORD=${google_secret_manager_secret.telemetry.secret_id}:${google_secret_manager_secret_version.telemetry_db.version}"
       ]
     }
   }
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,6 +2,9 @@ provider "google" {
   project = var.project_id
 }
 
+data "google_project" "project" {
+}
+
 # Bucket name is unfortunately hard-coded
 terraform {
   required_version = ">=1.3"
